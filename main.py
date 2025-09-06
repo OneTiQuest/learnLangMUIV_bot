330132b7a1eecbfa0f83940a93d44c84797a7956
mu.artushihin <mu.artushihin@tensor.ru>
2025-09-06 15:45:13 +0300

    Подключение к БД

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import psycopg2
 import telebot as tb
 from telebot import types
 
 TOKEN = "8271309227:AAH22j-4-MzFHekEKSFECDBtyP05_3MC0yY"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+DB_NAME = "postgres"
+
 bot = tb.TeleBot(TOKEN)
+conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASSWORD, host="localhost", port=5432)
+
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
@@ -13,4 +20,7 @@ def start(message):
         "Привет"
     )
 
-bot.infinity_polling()
+
+# Запуск бота
+if __name__ == "__main__":
+    bot.infinity_polling()
